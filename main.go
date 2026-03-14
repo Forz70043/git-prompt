830023156bde7a83a8d4c3337be43e5f71c1a268
Forz70043 <alfonso.pisicchio@libero.it>
2026-03-14 23:04:43 +0100

    core: added logic

---
+++ main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+	"fmt"
+	"os/exec"
+	"strings"
+)
+
+func main() {
+	// Exec command for branch
+	out, err := exec.Command("git", "rev-parse", "--abbrev-ref", "HEAD").Output()
+	
+	// If err != nil not repository git
+	if err != nil {
+		return
+	}
+
+	branch := strings.TrimSpace(string(out))
+	
+	if branch != "" {
+		fmt.Printf("git:%s", branch)
+	}
+}
